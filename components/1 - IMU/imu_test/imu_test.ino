faf57bed06232ed82f211a1572b7208a09cfe8d7
xcadush <49373293+xcadush@users.noreply.github.com>
2022-11-08 19:20:19 +0100

    Create imu_test.ino
    first test on conditions, everything ok

---
+++ components/1 - IMU/imu_test/imu_test.ino
@@ -0,0 +1,62 @@
+/*----- LIBRARIES -----*/
+#include <cmath>
+#include <Arduino_LSM9DS1.h>  // libreria per sensore IMU
+
+/*----- GLOBAL VARIABLES ----- */
+  /* UTILITIES AND FLAGS */
+  int count = 0;            // if count == VALUE_TBD then is_falling = true
+  bool is_falling = false;  
+
+/*---------------------*/
+/*----- FUNCTIONS -----*/
+
+void setup() {
+/*----- GENERAL SETUP -----*/
+  delay(2000);  // wait for serial door connection
+  Serial.begin(9600); // open serial door, set framerate @ 9600 bps
+  while (!Serial);
+  Serial.println("Started");
+
+/*----- IMU SETUP -----*/
+  if (!IMU.begin()) {
+    Serial.println("Failed to initialize IMU!");
+    while (1);
+  }
+
+}
+
+void loop() {
+
+/*----- VARIABLES -----*/
+/* IMU */
+  float ax, ay, az; // accelerometer
+  float gx, gy, gz; // gyroscope
+
+/*----- COLLECT IMU DATA -----*/
+  if (IMU.accelerationAvailable() && IMU.gyroscopeAvailable()) {
+    IMU.readAcceleration(ax, ay, az);
+    IMU.readGyroscope(gx, gy, gz);
+    
+    Serial.print("ax:");  Serial.print(ax); Serial.print("\t");
+    Serial.print("ay:");  Serial.print(ay); Serial.print("\t");
+    Serial.print("az:");  Serial.print(az); Serial.print("\t\t");
+    Serial.print("gx:");  Serial.print(gx); Serial.print("\t");
+    Serial.print("gy:");  Serial.print(gy); Serial.print("\t");
+    Serial.print("gz:");  Serial.println(gz);
+  }
+
+  if(az < 0){
+    count ++;
+    if(count == 50){
+      is_falling = true;
+    }
+  }
+  else
+    count = 0;
+
+  if(is_falling){
+    Serial.println("RILASCIO PARACADUTE");
+    while(1);
+  }
+
+}
