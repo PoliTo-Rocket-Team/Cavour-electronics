e9596bf68db733a6588f61960d681f4fbe157eb2
SalvatoreLag <113727727+SalvatoreLag@users.noreply.github.com>
2022-11-22 23:54:21 +0100

    test: Multiple Serial Port Inizializations
    LoRa module and GPS module connected on non native UART interfaces

---
+++ LoRa-GPS Test/LoRa_GPS.ino
@@ -0,0 +1,123 @@
+#include "Arduino.h"
+#include "LoRa_E220.h"
+#include <TinyGPS.h>
+
+UART LoRa_Serial(digitalPinToPinName(6), digitalPinToPinName(5), NC, NC);// Arduino TX RX, respectively
+UART GPS_Serial(digitalPinToPinName(10), digitalPinToPinName(9), NC, NC);  // Arduino TX RX, respectively
+
+LoRa_E220 e220ttl(&LoRa_Serial, 2, 3, 4); //  RX AUX M0 M1
+TinyGPS gps;
+ 
+void printParameters(struct Configuration configuration);
+void printModuleInformation(struct ModuleInformation moduleInformation);
+
+void setup() {
+  Serial.begin(9600);
+  while(!Serial){};
+  delay(500);
+  GPS_Serial.begin(9600);
+
+  Serial.println();
+
+  // Startup all pins and UART
+  e220ttl.begin();
+
+  ResponseStructContainer c;
+  c = e220ttl.getConfiguration();
+  // It's important get configuration pointer before all other operation
+  Configuration configuration = *(Configuration*) c.data;
+  Serial.println(c.status.getResponseDescription());
+  Serial.println(c.status.code);
+
+  printParameters(configuration);
+
+  ResponseStructContainer cMi;
+  cMi = e220ttl.getModuleInformation();
+  // It's important get information pointer before all other operation
+  ModuleInformation mi = *(ModuleInformation*)cMi.data;
+
+  Serial.println(cMi.status.getResponseDescription());
+  Serial.println(cMi.status.code);
+
+  printModuleInformation(mi);
+}
+
+void loop() {
+  bool newData = false;
+  unsigned long chars;
+  unsigned short sentences, failed;
+
+  // For one second we parse GPS data and report some key values
+  for (unsigned long start = millis(); millis() - start < 1000;)
+  {
+    while (GPS_Serial.available())
+    {
+      char c = GPS_Serial.read();
+      // Serial.write(c); // uncomment this line if you want to see the GPS data flowing
+      if (gps.encode(c)) // Did a new valid sentence come in?
+        newData = true;
+    }
+  }
+
+  if (newData)
+  {
+    float flat, flon;
+    unsigned long age;
+    gps.f_get_position(&flat, &flon, &age);
+    Serial.print("LAT=");
+    Serial.print(flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flat, 6);
+    Serial.print(" LON=");
+    Serial.print(flon == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flon, 6);
+    Serial.print(" SAT=");
+    Serial.print(gps.satellites() == TinyGPS::GPS_INVALID_SATELLITES ? 0 : gps.satellites());
+    Serial.print(" PREC=");
+    Serial.print(gps.hdop() == TinyGPS::GPS_INVALID_HDOP ? 0 : gps.hdop());
+  }
+  
+  gps.stats(&chars, &sentences, &failed);
+  Serial.print(" CHARS=");
+  Serial.print(chars);
+  Serial.print(" SENTENCES=");
+  Serial.print(sentences);
+  Serial.print(" CSUM ERR=");
+  Serial.println(failed);
+  if (chars == 0)
+    Serial.println("** No characters received from GPS: check wiring **");
+
+}
+
+void printParameters(struct Configuration configuration) {
+  Serial.println("----------------------------------------");
+
+  Serial.print(F("HEAD : "));  Serial.print(configuration.COMMAND, HEX);Serial.print(" ");Serial.print(configuration.STARTING_ADDRESS, HEX);Serial.print(" ");Serial.println(configuration.LENGHT, HEX);
+  Serial.println(F(" "));
+  Serial.print(F("AddH : "));  Serial.println(configuration.ADDH, HEX);
+  Serial.print(F("AddL : "));  Serial.println(configuration.ADDL, HEX);
+  Serial.println(F(" "));
+  Serial.print(F("Chan : "));  Serial.print(configuration.CHAN, DEC); Serial.print(" -> "); Serial.println(configuration.getChannelDescription());
+  Serial.println(F(" "));
+  Serial.print(F("SpeedParityBit     : "));  Serial.print(configuration.SPED.uartParity, BIN);Serial.print(" -> "); Serial.println(configuration.SPED.getUARTParityDescription());
+  Serial.print(F("SpeedUARTDatte     : "));  Serial.print(configuration.SPED.uartBaudRate, BIN);Serial.print(" -> "); Serial.println(configuration.SPED.getUARTBaudRateDescription());
+  Serial.print(F("SpeedAirDataRate   : "));  Serial.print(configuration.SPED.airDataRate, BIN);Serial.print(" -> "); Serial.println(configuration.SPED.getAirDataRateDescription());
+  Serial.println(F(" "));
+  Serial.print(F("OptionSubPacketSett: "));  Serial.print(configuration.OPTION.subPacketSetting, BIN);Serial.print(" -> "); Serial.println(configuration.OPTION.getSubPacketSetting());
+  Serial.print(F("OptionTranPower    : "));  Serial.print(configuration.OPTION.transmissionPower, BIN);Serial.print(" -> "); Serial.println(configuration.OPTION.getTransmissionPowerDescription());
+  Serial.print(F("OptionRSSIAmbientNo: "));  Serial.print(configuration.OPTION.RSSIAmbientNoise, BIN);Serial.print(" -> "); Serial.println(configuration.OPTION.getRSSIAmbientNoiseEnable());
+  Serial.println(F(" "));
+  Serial.print(F("TransModeWORPeriod : "));  Serial.print(configuration.TRANSMISSION_MODE.WORPeriod, BIN);Serial.print(" -> "); Serial.println(configuration.TRANSMISSION_MODE.getWORPeriodByParamsDescription());
+  Serial.print(F("TransModeEnableLBT : "));  Serial.print(configuration.TRANSMISSION_MODE.enableLBT, BIN);Serial.print(" -> "); Serial.println(configuration.TRANSMISSION_MODE.getLBTEnableByteDescription());
+  Serial.print(F("TransModeEnableRSSI: "));  Serial.print(configuration.TRANSMISSION_MODE.enableRSSI, BIN);Serial.print(" -> "); Serial.println(configuration.TRANSMISSION_MODE.getRSSIEnableByteDescription());
+  Serial.print(F("TransModeFixedTrans: "));  Serial.print(configuration.TRANSMISSION_MODE.fixedTransmission, BIN);Serial.print(" -> "); Serial.println(configuration.TRANSMISSION_MODE.getFixedTransmissionDescription());
+
+
+  Serial.println("----------------------------------------");
+}
+void printModuleInformation(struct ModuleInformation moduleInformation) {
+  Serial.println("----------------------------------------");
+  Serial.print(F("HEAD: "));  Serial.print(moduleInformation.COMMAND, HEX);Serial.print(" ");Serial.print(moduleInformation.STARTING_ADDRESS, HEX);Serial.print(" ");Serial.println(moduleInformation.LENGHT, DEC);
+
+  Serial.print(F("Model no.: "));  Serial.println(moduleInformation.model, HEX);
+  Serial.print(F("Version  : "));  Serial.println(moduleInformation.version, HEX);
+  Serial.print(F("Features : "));  Serial.println(moduleInformation.features, HEX);
+  Serial.println("----------------------------------------");
+}
