79aa72da486d00cdc958568dc84a272d607515bb
xcadush <giacomo.luccisano@studenti.polito.it>
2023-04-27 09:42:47 +0200

    edit to test sketches

--- tests/samuele_test_sender_receiver/lora_receiver/lora_receiver.ino
+++ tests/samuele_test_sender_receiver/lora_receiver/lora_receiver.ino
@@ -1,6 +1,5 @@
 #include "Arduino.h"
 #include "LoRa_E220.h"
-#define ENABLE_RSSI true
 
 LoRa_E220 e220ttl(&Serial1, 2, 4, 6); //  RX AUX M0 M1
 
@@ -8,7 +7,6 @@ void setup() {
 
   Serial.begin(9600);
   e220ttl.begin();
-  pinMode(8, OUTPUT);
   delay(500);
 
 }
@@ -17,26 +15,16 @@ void loop() {
 
   if (e220ttl.available() > 1) {
 
-    #ifdef ENABLE_RSSI 
-	    ResponseContainer rc = e220ttl.receiveMessageRSSI();
-    #endif
+    ResponseContainer rc = e220ttl.receiveMessage();
 
     Serial.println(rc.status.getResponseDescription());    
     Serial.println(rc.data); 
     
-    Serial.print("RSSI: "); 
-    Serial.println(rc.rssi, DEC);
-    Serial.println();
-
-    digitalWrite(8, LOW);    
-
   } else {
     Serial.println("Waiting...");
     Serial.println();
-
-    digitalWrite(8, HIGH);
   }
 
-  delay(1000);
+  delay(350);
 
 }
---
+++ tests/samuele_test_sender_receiver/lora_sender/lora_sender.ino
@@ -0,0 +1,28 @@
+#include "Arduino.h"
+#include "LoRa_E220.h"
+
+LoRa_E220 e220ttl(&Serial1, 2, 4, 6); //  RX AUX M0 M1
+
+void setup() {
+  Serial.begin(9600);
+  e220ttl.begin();
+  delay(500);  
+}
+
+int i = 0;
+
+void loop() {
+
+  String msg;
+  msg = String(i);
+
+  ResponseStatus rs = e220ttl.sendMessage(msg);
+  Serial.print("Attempt n. ");
+  Serial.print(msg);
+  Serial.print("\t -> \t");
+  Serial.println(rs.getResponseDescription());
+
+  i += 1;
+  
+  delay(400);
+}
