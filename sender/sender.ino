5067ee8a6b1bd3474a2c008e4a08871b491e4408
SalvatoreLag <113727727+SalvatoreLag@users.noreply.github.com>
2023-06-07 00:33:22 +0200

    chore: updated with tested modifications

--- sender/sender.ino
+++ sender/sender.ino
@@ -8,7 +8,7 @@
 
 #define SAMPLE_DELAY 50
 #define SEND_TIMEOUT 500
-#define COM_DELAY 100
+#define COM_DELAY 300
 #define OUTPUT_FILE "log.txt"
 #define TX_LED 5
 #define RX_LED 6
@@ -36,14 +36,14 @@ float gx, gy, gz;
 float bar, temp;
 
 char data_line[140];
-bool old;
 
 LoRa_E220 e220ttl(&Serial1, 4, 2, 3); //  RX AUX M0 M1
 Adafruit_BMP280 bmp1;
 Adafruit_BMP280 bmp2;
-File myFile;
 
+File myFile;
 ResponseContainer incoming;
+ResponseStatus rs;
 
 
 void setup() {
@@ -54,9 +54,14 @@ void setup() {
   Serial.begin(9600);   // LOG - TO BE ELIMINATED 
   while(!Serial);
 
-  
   Serial1.begin(9600);
-  e220ttl.begin();
+  
+  if (!e220ttl.begin()) {
+    digitalWrite(TX_LED, HIGH);
+    digitalWrite(RX_LED, HIGH);
+    digitalWrite(SD_LED, HIGH);
+    while (1);
+  }
 
   if (!SD.begin(A0)) {
     Serial.println("Errore di inizializzazione della scheda SD");   // LOG - TO BE ELIMINATED
@@ -98,16 +103,41 @@ void setup() {
     }
   }
 
-  if(myFile = SD.open(OUTPUT_FILE, FILE_WRITE)) Serial.println("File opened!");   // LOG - TO BE ELIMINATED
+  if(!(myFile = SD.open(OUTPUT_FILE, FILE_WRITE))) {
+    Serial.println("Could not open file in setup :(");    // LOG - TO BE ELIMINATED 
+    while(1) {
+      digitalWrite(SD_LED, HIGH);
+      delay(4000);
+      digitalWrite(SD_LED, LOW);
+      delay(4000);
+    }
+  }
+
+  ResponseStructContainer c = e220ttl.getConfiguration();
+  Configuration config = *(Configuration*) c.data;
+  c.close();
+  Serial.print("Old saved channel: ");   // LOG - TO BE ELIMINATED
+  Serial.println(config.CHAN);   // LOG - TO BE ELIMINATED
+  delay(200);   // LOG - TO BE ELIMINATED
+  Serial.println("Setting default frequency");   // LOG - TO BE ELIMINATED
+  config.CHAN = 23;
+  delay(500);
+  rs = e220ttl.setConfiguration(config, WRITE_CFG_PWR_DWN_SAVE);
+  Serial.println(rs.getResponseDescription());   // LOG - TO BE ELIMINATED
+  Serial.println();   // LOG - TO BE ELIMINATED
+  delay(500);
 
   incoming.data = "A";
   while(incoming.data[0] != 'C') {
     if(e220ttl.available())
       incoming = e220ttl.receiveMessage();
-    else {
-      Serial.println("Send [C]");   // LOG - TO BE ELIMINATED
+      Serial.print("I received something:");   // LOG - TO BE ELIMINATED
+      Serial.println(incoming.data);   // LOG - TO BE ELIMINATED
+    else {  
       packet.code = 'C';
-      e220ttl.sendMessage(&packet, sizeof(RocketData));
+      rs = e220ttl.sendMessage(&packet, sizeof(RocketData));
+      Serial.println("Transmitting check");   // LOG - TO BE ELIMINATED
+      Serial.println(rs.getResponseDescription());   // LOG - TO BE ELIMINATED
     }
     delay(COM_DELAY);
   }
@@ -116,6 +146,7 @@ void setup() {
 void loop() {
   static unsigned long last_send = millis();
   static unsigned long elapsed;
+  bool old;
 
   if(e220ttl.available()) {
     Serial.print("Message received -> ");   // LOG - TO BE ELIMINATED
@@ -136,7 +167,6 @@ void loop() {
     }
   }
 
-  // get sensor data, format string, write to SD
   old = true;
   readPT();
   if (IMU.accelerationAvailable() && IMU.gyroscopeAvailable()) {
@@ -155,8 +185,12 @@ void loop() {
   elapsed = last_send - millis();
   if (elapsed > SEND_TIMEOUT) {
     last_send = millis();
-    if (myFile) myFile.flush();
+    if (myFile) {
+      myFile.flush();
+      Serial.println("SD flushed");   // LOG - TO BE ELIMINATED
+    } 
     sendData();
+    Serial.println("Data transmitted");   // LOG - TO BE ELIMINATED
   }
   delay(SAMPLE_DELAY); 
 }
